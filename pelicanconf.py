3820d4d0ae57f305cb7b29c14e1369a499291ff5
40923221 <40923221@gm.nfu.edu.tw>
2022-03-31 14:59:27 +0800

    leo title updata

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = 'cd2022bg4'
+SITENAME = 'CD2022BG4 網際內容管理'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
